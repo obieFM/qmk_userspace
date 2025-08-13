d0baf8d588d8390b202a2e5533854325d648c953
obie <107162342+obieFM@users.noreply.github.com>
2025-08-12 23:23:18 -0500

    Add files via upload

--- keyboards/keebio/iris/keymaps/obie/keymap.c
+++ keyboards/keebio/iris/keymaps/obie/keymap.c
@@ -39,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PSCR,  KC_F12
     ),
     [3] = LAYOUT(
-        KC_6,    KC_7,    KC_8,     KC_9,    KC_0,     KC_BSPC,                      QK_GESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,
-        KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_DEL,                       KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,
-        KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_QUOT,                      KC_LALT,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,
-        KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  XXXXXXX,  XXXXXXX,  KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,
-                                             KC_LGUI,  KC_ENT,   XXXXXXX,  XXXXXXX,  KC_SPC,   KC_RCTL
+        KC_BSPC,  KC_6,  KC_7,  KC_8,     KC_9,     KC_0,                         KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  QK_GESC,
+        KC_DEL,   KC_Y,  KC_U,  KC_I,     KC_O,     KC_P,                         KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_TAB,
+        KC_QUOT,  KC_H,  KC_J,  KC_K,     KC_L,     KC_SCLN,                      KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_LALT,
+        KC_RSFT,  KC_N,  KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  XXXXXXX,  XXXXXXX,  KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,  KC_LSFT,
+                                          KC_RGUI,  KC_ENT,   XXXXXXX,  XXXXXXX,  KC_SPC,  KC_RCTL
     )
 };
 
